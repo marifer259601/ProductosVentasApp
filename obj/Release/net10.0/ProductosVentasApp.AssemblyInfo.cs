c1c2fcfe4b1c4e1554ddf9647af3e2ab09f31317
marifer259601 <marifer2002m@gmail.com>
2026-06-18 20:01:16 -0500

    Agrega Jenkinsfile

--- obj/Release/net10.0/ProductosVentasApp.AssemblyInfo.cs
+++ obj/Release/net10.0/ProductosVentasApp.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductosVentasApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cd3c8189f683b31b38e37019ce41a24aed926cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductosVentasApp")]
